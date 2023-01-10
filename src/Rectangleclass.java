3830132d4c2f7d38b5899c41a66e44f65497f20f
trieunhuyenquoc <trieunguyenquoc020604@gmai.com>
2023-01-10 11:53:16 +0700

    https://github.com/TrieuNguyenQuoc/lopvadoituongjava.git

---
+++ src/Rectangleclass.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Rectangleclass {
+        double width, height;
+
+        public Rectangleclass() {
+        }
+        public Rectangleclass(double width, double height) {
+            this.width = width;
+            this.height = height;
+        }
+
+        public double getArea() {
+            return this.width * this.height;
+        }
+
+        public double getPerimeter() {
+            return (this.width + this.height) * 2;
+        }
+
+        public String display() {
+            return "Rectangle{" + "width=" + width + ", height=" + height + "}";
+        }
+    }
+
+
+
+
